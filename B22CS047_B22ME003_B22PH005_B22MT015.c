5e9f814871de2d5b505397ab35ca30900a0e71d8
Aditya-continuum <120390919+Aditya-continuum@users.noreply.github.com>
2023-12-01 22:03:45 +0530

    B22CS047_B22ME003_B22PH005_B22MT015.c

--- B22CS047_B22ME003_B22PH005_B22MT015.c
+++ B22CS047_B22ME003_B22PH005_B22MT015.c
@@ -2,10 +2,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include <windows.h>
 void delay(int seconds);
-//main fn starts now here
-//efirnging
+//main fn starts here
 int main()
 {
   int timer,current,i=0;
@@ -16,7 +14,7 @@ int main()
   if(timer<=0)
   {
     printf("Wrong input,Please enter a positive integer.");
-    return 0;
+    return 2;
   }
 
   //to initialize the starting time and ending time
@@ -25,26 +23,14 @@ int main()
 //to display the countdown
    while(time(NULL)<stop)
      {
-        delay(1);
-        
-        
        current=stop-time(NULL);
-       PlaySound("demo.wav", NULL, SND_ASYNC);
-    //    if(current<=3){
-    //     MessageBeep(MB_OK);
-    //    }
-    //    else{
-    //     MessageBeep(MB_ICONINFORMATION);
-    //     // MessageBeep(MB_ICONQUESTION);
-    //    }
-    //    printf("\a");
-       printf("Remaining time: %d s\n",current+1);
+       printf("\a");
+       printf("Remaining time: %d s\n",current);
+       delay(1);
      }
-    for(int i=0;i<250;i++)
-    {
-        printf("\a");
-    }
+   
     printf("Times up!");
+    printf("\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a");
   return 0;
 }
 void delay(int seconds)
@@ -53,5 +39,5 @@ void delay(int seconds)
   while(time(NULL)-starting_time<seconds)
     {
       //just for passing the time required
-      }
+}
 }
